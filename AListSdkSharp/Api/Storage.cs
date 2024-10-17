730b0d9c5beca8252f734e4777aa7a34a490afab
jx <jx@jvxiang.com>
2024-10-17 08:34:24 +0800

    增加启用存储接口

--- AListSdkSharp/Api/Storage.cs
+++ AListSdkSharp/Api/Storage.cs
@@ -15,11 +15,19 @@ namespace AListSdkSharp.Api
             _baseUri = new Uri(baseUrl);
         }
         
-        public Task<StorageListOut> List(string token, int page, int prePage, CancellationToken cancellationToken = default)
+        public Task<StorageListOut> List(string token, int? page = null, int? prePage = null, CancellationToken cancellationToken = default)
         {
             return new Uri(_baseUri, $"/api/admin/storage/list?page={page}&prePage={prePage}")
                 .WithHeader("Authorization", token)
                 .GetJsonAsync<StorageListOut>(cancellationToken: cancellationToken);
+        }
+        
+        public Task<Base> Enable(string token, int id, CancellationToken cancellationToken = default)
+        {
+            return new Uri(_baseUri, $"/api/admin/storage/enable?id={id}")
+                .WithHeader("Authorization", token)
+                .PostAsync(cancellationToken: cancellationToken)
+                .ReceiveJson<Base>();
         }
     }
 }
