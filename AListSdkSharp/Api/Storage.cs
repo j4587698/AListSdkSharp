1dfcb7fb605f125c2992258fccb341786db7d055
jx <jx@jvxiang.com>
2024-10-17 08:27:59 +0800

    增加列出存储列表接口

---
+++ AListSdkSharp/Api/Storage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AListSdkSharp.Vo;
+using Flurl.Http;
+
+namespace AListSdkSharp.Api
+{
+    public class Storage
+    {
+        private readonly Uri _baseUri;
+        
+        public Storage(string baseUrl)
+        {
+            _baseUri = new Uri(baseUrl);
+        }
+        
+        public Task<StorageListOut> List(string token, int page, int prePage, CancellationToken cancellationToken = default)
+        {
+            return new Uri(_baseUri, $"/api/admin/storage/list?page={page}&prePage={prePage}")
+                .WithHeader("Authorization", token)
+                .GetJsonAsync<StorageListOut>(cancellationToken: cancellationToken);
+        }
+    }
+}
