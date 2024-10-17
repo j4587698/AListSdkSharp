26d39d808a5b2d3d74594b865e4fa0f9a74c6495
jx <jx@jvxiang.com>
2024-10-17 08:38:51 +0800

    增加注释

--- AListSdkSharp/Api/Storage.cs
+++ AListSdkSharp/Api/Storage.cs
@@ -15,6 +15,14 @@ namespace AListSdkSharp.Api
             _baseUri = new Uri(baseUrl);
         }
         
+        /// <summary>
+        /// 获取所有存储列表
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="page">页码，默认为null</param>
+        /// <param name="prePage">每页条数，默认为null</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public Task<StorageListOut> List(string token, int? page = null, int? prePage = null, CancellationToken cancellationToken = default)
         {
             return new Uri(_baseUri, $"/api/admin/storage/list?page={page}&prePage={prePage}")
@@ -22,6 +30,13 @@ namespace AListSdkSharp.Api
                 .GetJsonAsync<StorageListOut>(cancellationToken: cancellationToken);
         }
         
+        /// <summary>
+        /// 启用存储
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="id">存储id，从list中获取</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public Task<Base> Enable(string token, int id, CancellationToken cancellationToken = default)
         {
             return new Uri(_baseUri, $"/api/admin/storage/enable?id={id}")
@@ -30,6 +45,13 @@ namespace AListSdkSharp.Api
                 .ReceiveJson<Base>();
         }
         
+        /// <summary>
+        /// 禁用存储
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="id">存储id，从list中获取</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public Task<Base> Disable(string token, int id, CancellationToken cancellationToken = default)
         {
             return new Uri(_baseUri, $"/api/admin/storage/disable?id={id}")
